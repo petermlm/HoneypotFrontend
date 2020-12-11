9af808e04b9423550817487ec51f230738b152f6
Pedro Melgueira <pedromelgueira@gmail.com>
2020-12-11 15:07:17 +0100

    Adds epic mood

--- src/App.js
+++ src/App.js
@@ -4,10 +4,20 @@ import ConnAttemps from './ConnAttemps.js';
 import Map from './Map.js';
 
 class App extends Component {
+  mood = () => {
+    var player = document.getElementById('player');
+    if(player.paused) {
+      player.play();
+    } else {
+      player.pause();
+    }
+  }
+
   render() {
     return (
       <div className="App">
-        <h1 className="App-Title">Get some of that sweet&nbsp;<span id="App-Title-Honey">Honey</span>!</h1>
+        <h1 className="App-Title">Get some of that sweet&nbsp;<span className="App-Honey">Honey</span>!</h1>
+        <p className="App-Mood"><a href="#" onClick={this.mood}>get in the&nbsp;<span className="App-Honey">mood</span></a></p>
         <Map />
         <ConnAttemps />
       </div>
